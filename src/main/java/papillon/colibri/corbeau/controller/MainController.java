8d6c96a0ddfaa812e84a9c45e5c6dc11bcffb0a2
villiseck <villiseck@abes.fr>
2023-10-10 16:37:16 +0200

    Ajout d'un Controller Main pour accès rapide

---
+++ src/main/java/papillon/colibri/corbeau/controller/MainController.java
@@ -0,0 +1,12 @@
+package papillon.colibri.corbeau.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String home(){
+        return "hello";
+    }
+}
