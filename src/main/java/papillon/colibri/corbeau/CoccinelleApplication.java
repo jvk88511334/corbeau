40bee8407774be1298f7cca85bbbb7bc9b14b294
villiseck <villiseck@abes.fr>
2023-10-10 15:08:56 +0200

    Initial commit

---
+++ src/main/java/papillon/colibri/corbeau/CoccinelleApplication.java
@@ -0,0 +1,13 @@
+package papillon.colibri.corbeau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoccinelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoccinelleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/papillon/colibri/corbeau/CoccinelleApplicationTests.java
@@ -0,0 +1,13 @@
+package papillon.colibri.corbeau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoccinelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
