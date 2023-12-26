9d0ea0d408b3c71c768b51e8b35cc803eb07f041
Jerome Villiseck <jerome.villiseck@outlook.com>
2023-12-26 16:22:31 +0100

    CRUD en JDBC classique, avec tables grillon, patte, panier possedant des jointures, crud placé dans classe de test JdbcTemplateRequestsTests

--- src/test/java/papillon/colibri/corbeau/repository/JdbcTemplateRequestsTests.java
+++ src/test/java/papillon/colibri/corbeau/repository/JdbcTemplateRequestsTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -21,14 +22,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Test CRUD avec JDBC brut (JDBC Template) - bas niveau
  */
 
-@Slf4j
-@SpringBootTest
-@ActiveProfiles("test")
+@Slf4j //journalisation simplifiée avec lombok (requiers la dépendance lombok dans le pom)
+@SpringBootTest //crée un contexte d'application et charge un contexte d'application complet pour les tests, permet notamment les @Autowired dans une classe de test (dépendance spring-boot-starter-test)
+@ActiveProfiles("test") //seul les beans associés au profil “test” seront instanciés dans le contexte d’application pour les tests
 @TestPropertySource("classpath:application-repository-test.properties") //Branchement sur une base de donnée de test
 class JdbcTemplateRequestsTests {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    //Test d'insertion simple avec JDBC sans jointure
     @Test
     void testInsertWithJdbcTemplateRequest() {
         //Insertion de la donnée
@@ -46,7 +48,7 @@ class JdbcTemplateRequestsTests {
                 return sauterelleEntity;
             }
         });
-        log.info(sauterelleEntities.toString());
+        this.console(sauterelleEntities.toString());
 
         //Suppression de la donnée
         int rowsCancelled = jdbcTemplate.update("DELETE FROM SAUTERELLE WHERE ID=?", -1);
@@ -63,6 +65,43 @@ class JdbcTemplateRequestsTests {
                 return sauterelleEntity;
             }
         });
-        log.info(sauterelleEntities2.toString());
+        this.console(sauterelleEntities2.toString());
     }
+
+    //Test d'insertion complexe avec JDBC avec jointure
+    @Test
+    void testInsertDataWithJoinsWithJdbcTemplateRequest() {
+        //Creation d'un panier
+        jdbcTemplate.update("INSERT INTO panier (ID, NEUF) VALUES(3, false)");
+        //Insertion de la jointure : création et rattachement du grillon au panier n°3
+        jdbcTemplate.update("INSERT INTO grillon (ID, COULEUR, NAISSANCE, ID_1, ID_2) VALUE (?,?,?,?,?)", -1, "brun", new Date(2023, 12, 01), null, 3);
+        //Insertion de la jointure : création d'une patte rattachée au grillon portant l'id -1
+        jdbcTemplate.update("INSERT INTO patte (ID, ID_1) VALUE (?, ?)", 1,-1);
+
+        //Rajout d'un grillon noir avec l'id -2 sans pattes en couple avec le grillon brun, dans le panier n°3
+        jdbcTemplate.update("INSERT INTO grillon (ID, COULEUR, NAISSANCE, ID_1, ID_2) VALUE (?,?,?,?,?)", -2, "noir", new Date(2023, 10, 05), -1, 3);
+
+        //Log du grillon avec les jointures, utilisation d'un RowCallBackHandler en lambda pour ne pas utiliser d'objet intermédiaire (pas d'entité nécessaire par rapport à Sauterelle)
+        jdbcTemplate.query("SELECT * FROM GRILLON", rs -> {
+            console(rs.toString());
+            do{
+                console("ID "+rs.getString(1));
+                console("COULEUR "+rs.getString(2));
+                console("ID_1=grillon(ID) "+rs.getString(4));
+                console("ID_2=panier(ID) "+rs.getString(5));
+                console("");
+            } while (rs.next());
+        });
+
+        //Respect des contraintes d'intégrités pour la suppression des données (ordre des suppressions)
+        jdbcTemplate.update("DELETE from patte where ID=?", 1);
+        jdbcTemplate.update("DELETE from grillon where ID=?", -2);
+        jdbcTemplate.update("DELETE from grillon where ID=?", -1);
+        jdbcTemplate.update("DELETE from panier where ID=?", 3);
+    }
+
+    private void console(String stringToColor){
+        log.info("\u001B[34m" + "\u001B[1m" + stringToColor + "\u001B[0m");
+    }
+
 }
