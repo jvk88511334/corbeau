9a7e70b0167859925af09632b3f138da497b5811
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-02-28 17:16:38 +0100

    FONCTIONNEL : Suppression de tous les éléments non relatifs à un projet uniquement JDBC

--- src/main/java/papillon/colibri/corbeau/repository/CoursRepository.java
+++
@@ -1,6 +0,0 @@
-package papillon.colibri.corbeau.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import papillon.colibri.corbeau.entity.CoursEntity;
-
-public interface CoursRepository extends CrudRepository<CoursEntity, Integer> {}
