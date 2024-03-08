02f0b569891a136544bd19661a64c68e9402834a
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-03-08 14:54:04 +0100

    VAADIN : Views : Ajout d'une vue avec une route basique

--- src/main/java/papillon/colibri/corbeau/CoccinelleApplication.java
+++ src/main/java/papillon/colibri/corbeau/CoccinelleApplication.java
@@ -1,13 +1,12 @@
 package papillon.colibri.corbeau;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoccinelleApplication {
-
+public class CoccinelleApplication implements AppShellConfigurator {
 	public static void main(String[] args) {
 		SpringApplication.run(CoccinelleApplication.class, args);
 	}
-
 }
