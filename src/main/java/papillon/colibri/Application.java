40fefe0713ffdc41bb74adff5bcb1ac8a7e21d64
jvk <villiseck@abes.fr>
2024-03-29 13:44:04 +0100

    FONCTIONNEL : Fin de redaction du docker-compose.yml avec l'image postée sur le docker hub

--- src/main/java/papillon/colibri/Application.java
+++ src/main/java/papillon/colibri/Application.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import papillon.colibri.dao.SauterelleDao;
 
 import java.sql.SQLException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws SQLException {
         SpringApplication.run(Application.class, args);
-        SauterelleDao.getAllUsers().forEach(System.out::println);
+        Objects.requireNonNull(SauterelleDao.getAllUsers()).forEach(System.out::println);
     }
 }
